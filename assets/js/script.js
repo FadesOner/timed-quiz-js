f65525499dc4995437d95dffbf7e9043e23e73fb
Luis Debourg <Luisdebourgm@gmail.com>
2022-03-22 18:38:28 +1100

    added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,136 @@
 var timeEl = document.querySelector(".timer");
 var secondsLeft = 60;
 var choice = false;
-var answer1 = questions[0].choices[0].title;
-var answer2 = questions[0].choices[1].title;
-var answer3 = questions[0].choices[2].title;
-var answer4 = questions[0].choices[3].title;
 var isCorrect = true;
+var i;
 // import { questions } from './questions';
 //when i click on start button 
 const buttonStartQuiz = document.querySelector(".startq");
+const choiceList = document.getElementById('choice-list');
+
+const spanFeedback = document.getElementById('span-feedback');
+
 
 buttonStartQuiz.addEventListener("click", function(event){
-
+    
     // the 1 question pops up with the answers to choose
     showQuestion(0);
     //the timer start 
     startTimer();
-
+    
     //hide the main page
     document.getElementById('section-main-page').classList.add('hide');
     // show the quetion page
     document.getElementById('section-question').classList.remove('hide');
-
+    
 });
 
+
+
+function createButton(title, isCorrect){
+
+    const li = document.createElement('li');
+
+    const button = document.createElement('button');
+
+    button.classList.add('btn');
+
+    button.setAttribute('data-correct', isCorrect);
+    button.textContent = title;
+
+    li.append(button);
+
+    return li;
+
+}
+function showFeedback(isCorrect) {
+    let message;
+    if(isCorrect){
+        message = 'Correct!';
+    }else{
+        message = ' Wrogn!';
+    }
+
+    spanFeedback.textContent = message;
+    spanFeedback.classList.remove('hide');
+
+    // i want to show this for 1 sec
+
+    setTimeout(function(){
+        // rehide the span
+        spanFeedback.classList.add('hide');
+    }, 1000);
+
+    
+}
+
 function showQuestion(index){
- 
-document.getElementById("question-title").innerHTML = questions[0].title;
-document.getElementById("answer1").innerHTML = answer1
-document.getElementById("answer2").innerHTML = answer2
-document.getElementById("answer3").innerHTML = answer3
-document.getElementById("answer4").innerHTML = answer4
+    
+    function checkAnswer(event) {
+        event.preventDefault();
+    
+        const button = event.target;
+    
+        const isCorrect = button.getAttribute('data-correct') === 'true';
+    
+        // if right 
+        // do nothing
+        if(isCorrect){
+            showFeedback(true);
+        }
+        
+        // if wrong reduce time on timer
+        if(!isCorrect){
+            secondsLeft = secondsLeft - 10;
+            // pop up message say its wrong
+            showFeedback(false);
+        }
+        
+        // nmove to next question
+        showQuestion(index + 1);
+    
+    }
+
+
+    // what if index exceed total qs??
+    if(index >= questions.length){
+        return endGame();
+    }
+//stop the time 
+//
+    
+    var question = questions[index];
+    
+
+    var choices = question.choices;
+
+
+
+    // <li ><button id="answer1" data-correct="true" class="btn1">answer1</button></li>
+
+    // generate the choice button
+
+
+
+    choiceList.textContent ="";
+    for (let index = 0; index < choices.length; index++) {
+        const choice = choices[index];
+        var choiceButton = createButton(choice.title, choice.isCorrect);
+        // when clicked on the button
+        choiceButton.addEventListener('click', checkAnswer);
+        choiceList.append(choiceButton);
+    
+    }
+
+
+
+
+
+
+document.getElementById("question-title").innerHTML = question.title;
+
+
+
 
 
 }
@@ -44,7 +144,7 @@ function startTimer() {
     
 }
 
-answer1.addEventListener("click", showQuestion(0));
+
     
 
 // function answerQ() {
@@ -55,7 +155,6 @@ answer1.addEventListener("click", showQuestion(0));
 //     textContent = "wrong answer"
 // }
 // }
-for (var i = 0; i < questions.length; i++){
-    showQuestion(i);
-}
-console.log(showQuestion);
+
+
+
